f2f789ae1c0a91a353eac495712f6bb7eefac9d3
Arthur Caumartin <103814696+ArthurCaumartin@users.noreply.github.com>
2025-10-01 19:20:24 +0200

    awd

--- Kare/Assets/[Project]/Scripts/Paterne/Paterne.cs
+++ Kare/Assets/[Project]/Scripts/Paterne/Paterne.cs
@@ -5,7 +5,7 @@ public class Paterne : MonoBehaviour
     [SerializeField] protected int damage = 10;
     protected Camera _camera;
     protected Transform player;
-    
+
     public virtual void Init(Camera camera, GameObject player)
     {
         this._camera = camera;
@@ -35,7 +35,6 @@ public class Paterne : MonoBehaviour
         Vector3 worldPosition = _camera.ScreenToWorldPoint(screenPosition);
         return new Vector2(worldPosition.x, worldPosition.y);
     }
-
     protected Vector2 GetPosOutCamera()
     {
         float screenX = Random.Range(0, Screen.width);
@@ -60,4 +59,30 @@ public class Paterne : MonoBehaviour
         Vector3 worldPosition = _camera.ScreenToWorldPoint(screenPosition);
         return new Vector2(worldPosition.x, worldPosition.y);
     }
+}
+
+
+public class Paterne_Crescent : Paterne
+{
+    [SerializeField] private float _moveSpeed = 5f;
+    [SerializeField] private float _rotateSpeed = 5f;
+    [SerializeField] private float _centerOffsetRadius = 2f;
+    private Vector2 _target;
+
+    public override void Init(Camera camera, GameObject player)
+    {
+        base.Init(camera, player);
+        transform.position = GetPosOutCamera();
+
+        Vector2 centreOffset = Random.insideUnitCircle * _centerOffsetRadius;
+        
+    }
+
+    protected override void Start()
+    {
+        base.Start();
+        Destroy(gameObject, 15f);
+    }
+
+
 }
